8d1fd4b954d796211a0a6ebece4360c5aa65cb43
Anuj <122350668+anujbalak@users.noreply.github.com>
2025-03-14 19:39:11 +0530

    Create shopping page button and shopping page

---
+++ src/components/ShoppingBtn.jsx
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+    background-color: transparent;
+    border: none;
+    padding: 0.4em 1em;
+    color: snow;
+    background-color: rgba(84, 94, 243, 100%);
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 700;
+    transition: background-color ease-in-out 0.3s;
+    backdrop-filter: blur(10px);
+
+    &&:hover, &&:focus {
+        background-color: rgba(84, 94, 243, 70%);
+
+    }
+`
+
+class ShoppingBtn extends Component {
+
+    render() {
+        return(
+            <Link to="shopping">
+                <Button>
+                    Shop ➞
+                </Button>
+            </Link>
+        )
+    }
+}
+
+export default ShoppingBtn;
---
+++ src/web/Shopping.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import styled from "styled-components";
+
+const ShoppingContainer = styled.div`
+    
+`
+
+class Shopping extends Component {
+    
+
+    render() {
+        return (
+            <ShoppingContainer>Shopping page</ShoppingContainer>
+        )
+    }
+}
+
+export default Shopping;
